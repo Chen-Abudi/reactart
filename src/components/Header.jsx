a32e557481e00dfdd43a56dec2844c916ddba1e2
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-02-04 23:38:55 +0200

    feat(styles): Add media query to header and styled button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,22 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
       <img
         src={logo}
         alt="A canvas"
         className="object-contain mb-8 w-44 h-44"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactArt
       </h1>
       <p
-      // This is an inline styles
-      // style={{
-      //   color: "red",
-      //   textAlign: "left",
-      // }}
+        className="text-stone-500"
+        // This is an inline styles
+        // style={{
+        //   color: "red",
+        //   textAlign: "left",
+        // }}
       >
         A community of artists and art-lovers.
       </p>
